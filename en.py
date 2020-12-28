c9816737ecac41045cb83e536985525602ca8142
SamirAk <44325916+SamirPS@users.noreply.github.com>
2020-12-28 13:55:05 +0100

    Add foxnews

--- en.py
+++ en.py
@@ -6,3 +6,10 @@ def CNNSC():
   arcnn=re.findall(r'<link>(.*?)</link>',CNN.text)
   del arcnn[0]
   return arcnn
+
+def FoxNewsSC():
+  FoxNews=requests.get("http://feeds.foxnews.com/foxnews/latest")
+  arfoxnews=re.findall(r'<link>(.*?)</link>',FoxNews.text)
+  del arfoxnews[0]
+  del arfoxnews[0]
+  return arfoxnews
