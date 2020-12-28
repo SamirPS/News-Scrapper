4a7d550a3dcfd9ab1fe3d76cf5d14f432a29a467
SamirAk <44325916+SamirPS@users.noreply.github.com>
2020-12-28 13:10:38 +0100

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,35 +1,36 @@
 import requests
 import re
 
-Monde=requests.get("https://www.lemonde.fr/rss/une.xml")
-armonde=re.findall(r'<link>(.*?)</link>',Monde.text)
-del armonde[0]
-print(armonde)
+def MondeSC():
+  Monde=requests.get("https://www.lemonde.fr/rss/une.xml")
+  armonde=re.findall(r'<link>(.*?)</link>',Monde.text)
+  return armonde
 
-Paris=requests.get("https://www.leparisien.fr/arcio/sitemap/master/")
-arparis=re.findall(r'<loc>(.*?)</loc>',Paris.text)
-del arparis[0]
-print(arparis)
+def ParisSC():
+  Paris=requests.get("https://www.leparisien.fr/arcio/sitemap/master/")
+  arparis=re.findall(r'<loc>(.*?)</loc>',Paris.text)
+  return arparis
 
-MediaPart=requests.get("https://www.mediapart.fr/articles/feed")
-armediapart=re.findall(r'<link>(.*?)</link>',MediaPart.text)
-del armediapart[0]
-print(armediapart)
+def MediaPartSC():
+  MediaPart=requests.get("https://www.mediapart.fr/articles/feed")
+  armediapart=re.findall(r'<link>(.*?)</link>',MediaPart.text)
+  return armediapart
 
-Bfmtv=requests.get("https://www.bfmtv.com/rss/news-24-7/")
-arbfmtv=re.findall(r'<link>(.*?)</link>',Bfmtv.text)
-del arbfmtv[0]
-print(arbfmtv)
+def BmftvSC():
+  Bfmtv=requests.get("https://www.bfmtv.com/rss/news-24-7/")
+  arbfmtv=re.findall(r'<link>(.*?)</link>',Bfmtv.text)
+  return arbfmtv
 
-Liberation=requests.get("http://rss.liberation.fr/rss/latest/")
-arlibe=re.findall(r'<link href="(.*?)"',Liberation.text)
-del arlibe[0]
-print(arlibe)
+def LibeSC():
+  Liberation=requests.get("http://rss.liberation.fr/rss/latest/")
+  arlibe=re.findall(r'<link href="(.*?)"',Liberation.text)
+  return arlibe
 
-Francetvinfo=requests.get("https://www.francetvinfo.fr/titres.rss")
-arfti=re.findall(r'<link>(.*?)#',Francetvinfo.text)
-del arfti[0]
-print(arfti)
+def FTISC():
+  Francetvinfo=requests.get("https://www.francetvinfo.fr/titres.rss")
+  arfti=re.findall(r'<link>(.*?)#',Francetvinfo.text)
+  return arfti
+
 
 
 
