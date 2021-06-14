9b3b2b4d5f7ab2af8994861676338bdbaf4320ce
SamirAk <44325916+SamirPS@users.noreply.github.com>
2021-06-14 17:02:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="newsscrapper-SamirPS", # Replace with your own username
-    version="0.1.1",
+    version="0.1.2",
     author="SamirPS",
     long_description=long_description,
     long_description_content_type="text/markdown",
