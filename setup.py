2397adffd28e8876c5882def38bf612843cb580a
Samir Akarioh <samir.akarioh@canonical.com>
2022-12-08 13:28:24 +0100

    fix utils import

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup,find_packages
+from setuptools import setup
 from setuptools.command.install import install as _install
 
 
@@ -23,7 +23,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="newsscrapper-SamirPS",  # Replace with your own username
-    version="1.0.3",
+    version="1.0.4",
     author="SamirPS",
     long_description=long_description,
     long_description_content_type="text/markdown",
