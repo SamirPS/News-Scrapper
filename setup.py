4bb0752e70418fbc8a38d422f6e8e42ff90b35a4
Samir Akarioh <samir.akarioh@canonical.com>
2022-12-08 12:43:41 +0100

    feat: Add newspaper3k lib
    Thanks to this lib, we can now have the summary of article

--- setup.py
+++ setup.py
@@ -1,21 +1,42 @@
-import setuptools
+from setuptools import setup,find_packages
+from setuptools.command.install import install as _install
+
+import nltk
+
+class DownloadNLTK(install):
+    def run(self):
+        self.do_egg_install()
+        required_nltk = [
+            'brown',  # Required for FastNPExtractor
+            'punkt',  # Required for WordTokenizer
+            'maxent_treebank_pos_tagger',  # Required for NLTKTagger
+            'movie_reviews',  # Required for NaiveBayesAnalyzer
+            'wordnet',  # Required for lemmatization and Wordnet
+            'stopwords'
+        ]
+        for i in required_nltk:
+            nltk.download(i)
+
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="newsscrapper-SamirPS",  # Replace with your own username
-    version="1.0.1",
+    version="1.0.2",
     author="SamirPS",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/SamirPS/News-Scrapper",
-    install_requires=["requests", "bs4", "lxml"],
-    packages=setuptools.find_packages(),
+    install_requires=["requests", "bs4", "lxml","newspaper3k"],
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    cmdclass={'download_nltk': DownloadNLTK()},
 )
+
+
